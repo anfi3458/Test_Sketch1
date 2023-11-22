04bf7a7e1af686b5398470ae6543de9f554b5744
Andreas Fischer <151648137+anfi3458@users.noreply.github.com>
2023-11-22 14:10:39 +0100

    2. Upload
    keine Aenderung

---
+++ LED_Ansteuerung_OOP_3.2.ino
@@ -0,0 +1,29 @@
+/****************************************************************** Created with PROGRAMINO IDE for Arduino - 13.05.2018 14:52:44
+ Projekt:     LED_Ansteuerung_OOP3.2 
+              .1  abgeänderteVersion (nur 1 LED aber Eingabe Dauer und Pause)
+              .2  Klasse in Tab als include
+ Status:      ok 16.11.23
+ Libraries:   
+ Autor:       ich und Quelle
+ Quelle:      https://spacehal.github.io/docs/arduino/objectOriented  
+ Inhalt:      LED_BUILTIN soll als D4 bzw 2, Pause und Dauer blinken 
+*********************************************************************************************************************/
+#include "Led_Schlaufe.h"
+int x=1;
+void zaehle(){
+  x++;
+  Serial.println(x);
+}
+
+Led ledBlau(2, 1000,1000);  // ein `Led` Objekt mit dem Namen `ledGreen` mit Pin 4
+
+
+void setup() {
+  Serial.begin(115200);
+  // Rest bleibt leer
+}
+
+void loop() {
+  ledBlau.toggle();       // Methodenaufruf vom Objekt ledBlau 
+  zaehle();               //Funktion macht nur x++ und SeriMoni-Ausgabe
+}
